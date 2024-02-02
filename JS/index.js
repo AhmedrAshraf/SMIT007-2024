c6b22a82e5d74e58c542901dc14a4699aa56aa1c
AhmedrAshraf <ahmedr.0331@gmail.com>
2024-02-02 17:13:01 +0500

    javascript work

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+// var = variable
+
+// var email = "Ahmed"
+// alert(email)
+
+// BODMAS
+
+// var number1 = 4;
+// var number2 = 6;
+
+// var totalResult = number1 + number2 * number2 / number1
+// var easyTotal = number1 + number2
+
+// alert(easyTotal + " This is the Result to some easyTotal") // 10
+// alert(totalResult + " This is the Result to some totalResult") // 13
+
+// var userName = prompt("Please Enter Your Name!")
+// console.log(userName)
+
+var userAge = prompt("Please Enter Your Age!");
+var BirthMonth = prompt("Please Enter Your Birth Month!");
+var BirthDate = prompt("Please Enter Your Birth Date!");
+var birthYear = 2024 - userAge;
+
+alert("Your Birth Year is " + birthYear + "-" + BirthMonth + "-" + BirthDate)
