76b86c4adad325d9c7ac8a9ad1e3274935520693
AhmedrAshraf <ahmedr.0331@gmail.com>
2024-02-09 16:28:24 +0500

    new javascript class

---
+++ JS/9-2-24/index.js
@@ -0,0 +1,22 @@
+// var numberOne = +prompt("Please Enter first number")
+// var numberTwo = +prompt("Please Enter second number")
+
+// console.log("addition: ", numberOne + numberTwo); // 7
+// console.log("multiplication", numberOne * numberTwo); // 10
+// console.log("division", numberOne / numberTwo); // 2.5
+
+// var numberOne = 5
+// var numberTwo = 10
+// var newNumberOne = numberOne - 2 * 5
+// console.log("🚀 ~ newNumberOne:", Math.abs(newNumberOne))
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log("🚀 ~ resultOfComputation:", resultOfComputation)
+
+var rollNo = prompt("What is your Roll No!");
+
+if (rollNo == "55") {
+  alert("Your fee is submitted");
+} else {
+  alert("Your fee is not submitted");
+}
--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,11 @@
-// var = variable
-
-// var email = "Ahmed"
-// alert(email)
-
-// BODMAS
-
 // var number1 = 4;
 // var number2 = 6;
 
-// var totalResult = number1 + number2 * number2 / number1
 // var easyTotal = number1 + number2
+
+// console.log("easyTotal:", easyTotal)
+
+// var totalResult = number1 + number2 * number2 / number1
 
 // alert(easyTotal + " This is the Result to some easyTotal") // 10
 // alert(totalResult + " This is the Result to some totalResult") // 13
@@ -17,9 +13,9 @@
 // var userName = prompt("Please Enter Your Name!")
 // console.log(userName)
 
-var userAge = prompt("Please Enter Your Age!");
-var BirthMonth = prompt("Please Enter Your Birth Month!");
-var BirthDate = prompt("Please Enter Your Birth Date!");
-var birthYear = 2024 - userAge;
+// var userAge = prompt("Please Enter Your Age!");
+// var BirthMonth = prompt("Please Enter Your Birth Month!");
+// var BirthDate = prompt("Please Enter Your Birth Date!");
+// var birthYear = 2024 - userAge;
 
-alert("Your Birth Year is " + birthYear + "-" + BirthMonth + "-" + BirthDate)
+// alert("Your Birth Year is " + birthYear + "-" + BirthMonth + "-" + BirthDate)
